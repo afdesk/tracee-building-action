11f4bde09b3840f3589ab97ad8580fe622a84db7
AMF <work@afdesk.com>
2021-07-08 13:19:03 +0600

    Add actions

---
+++ action.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+wget https://github.com/afdesk/my-driverkit/releases/download/v0.5.3/driverkit_linux_x86_64.tar.gz
+tar -xf driverkit_linux_x86_64.tar.gz
+chmod +x driverkit
+./driverkit docker -c ubuntu.yaml --kernelrelease=$1 --output-probe=$2
