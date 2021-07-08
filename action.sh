54aae4f8a6f7b38e9436355bc01e1bd416b44e87
AMF <work@afdesk.com>
2021-07-08 13:45:32 +0600

    add custom path to config

--- action.sh
+++ action.sh
@@ -3,5 +3,5 @@ set -e
 wget https://github.com/afdesk/my-driverkit/releases/download/v0.5.3/driverkit_linux_x86_64.tar.gz
 tar -xf driverkit_linux_x86_64.tar.gz
 chmod +x driverkit
-echo "kernelrelease:$1 output-probe: $2"
-./driverkit docker -c ubuntu.yaml --kernelrelease=$1 --output-probe=$2
+echo "kernelrelease:$1 output-probe: $2 path: $3"
+./driverkit docker -c $3 --kernelrelease=$1 --output-probe=$2
